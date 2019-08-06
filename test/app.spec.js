bbeacb30b96b9066983008e506c514e8b5ca8d43
Christopher Williams <cw@chriswillia.ms>
2019-08-06 15:04:13 -0500

    Refactors the tests, adds tests for an empty table context, and refactors /article/:article_id end point to return a proper error if the article id does not exist.

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,6 +1,6 @@
 const app = require('../src/app');
 
-describe('App', () => {
+describe.skip('App', () => {
     it('GET / responds with 200 contaning "Hello, world!"', () => {
         return supertest(app)
             .get('/')
