49bf773ec12d3a6876f53b15f2016aa42f444f49
Christopher Williams <cw@chriswillia.ms>
2019-08-06 14:14:11 -0500

    Implements the GET enpoints for articles and some basic tests for them.

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
+const ArticlesService = require('./articles-service');
 
 const app = express();
 
@@ -20,6 +21,25 @@ app.use(function errorHandler(error, req, res, next) {
         response = { error };
     };
     res.status(500).json(response);
+});
+
+app.get('/articles', (req, res, next) => {
+    //res.send('All articles');
+    const knexInstance = req.app.get('db');
+    ArticlesService.getAllArticles(knexInstance)
+        .then(articles => {
+            res.json(articles);
+        })
+        .catch(next);
+});
+
+app.get('/articles/:article_id', (req, res, next) => {
+    const knexInstance = req.app.get('db');
+    ArticlesService.getById(knexInstance, req.params.article_id)
+        .then(article => {
+            res.json(article);
+        })
+        .catch(next);
 });
 
 app.get('/', (req, res) => {
